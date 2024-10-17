f696c34bd000e4eb04ebf3df770c1348060a15bd
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-17 15:21:56 -0300

    Update webapp05.py

--- webapp05.py
+++ webapp05.py
@@ -11,31 +11,6 @@ def main():
    Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
    image = Image.open('desenvolvimento.jpg')
    st.sidebar.image(image, width=300)
-	
-   CONTAINER = Container(True)
-   with CONTAINER: 
-      MKD('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessoa leiga na Programação.', "justified", 20, "black")
-    
-   Barra_Lateral_Texto("Desenvolvido por Massaki Igarashi")
 
-   colunas = Colunas(4)       
-    
-   with colunas[0]:
-   	BTN1 = Botao_M("✔️ Botão 1")
-   with colunas[1]:
-        BTN2 = Botao_C("✔️ Botão 2", "gray")
-   with colunas[2]:
-        BTN3 = Botao_M("Botão 3")
-   with colunas[3]:   
-        BTN4 = Botao_C("✔️ Botão 4")
-       
-   if BTN1:
-        Escrever("Você clicou no Botão 1")
-   if BTN2:
-        Escrever("Você clicou no Botão 2")
-   if BTN3:
-        Escrever("Você clicou no Botão 3")       
-   if BTN4:
-        Escrever("Você clicou no Botão 4") 
 if __name__ == '__main__':
 	main()
