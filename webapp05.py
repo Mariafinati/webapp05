28fa2111ff4211106d8bb18751a0791d4115cc37
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-17 15:20:40 -0300

    Update webapp05.py

--- webapp05.py
+++ webapp05.py
@@ -5,12 +5,7 @@ from Massaki01 import *
 def main():
    Configurar_Pagina("WebApp01 - TESTE", "amplo", "auto", "https://docs.streamlit.io", "mailto:prof.massaki@gmail.com", "#### Aplicativo Teste", "©️")
    Titulo("MEU PRIMEIRO APLICATIVO WEB")
-   st.header("Desenvolvido por Massaki de O. Igarashi")
-   
-   MKD(Versao(), 'left', 18, 'blue')
-      
-   MKD('⬜ Massaki01 - Biblioteca teste', 'left', 48, 'darkblue')
-      
+   st.header("Desenvolvido por Massaki de O. Igarashi") 
    Escrever('Hehehehe! Agora todos poderão fazer seu site e programar facilmente!', 'subcabecalho')
    Escrever(' ')
    Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
