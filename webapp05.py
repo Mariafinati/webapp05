b47dc6d4b693285d15a17cd3545d84ccdbd68f6a
Prof. Massaki <83902949+massakiigarashi2@users.noreply.github.com>
2024-10-17 15:17:51 -0300

    Update webapp05.py

--- webapp05.py
+++ webapp05.py
@@ -1,36 +1,46 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
-from ACTlib01 import *
+from PIL import Image
+from Massaki01 import *
 
-Configurar_Pagina("Exemplo 1", 
-                    "amplo", 
-                    "auto", 
-                    "https://docs.streamlit.io", 
-                    "mailto:informacoes.actsp@gmail.com", 
-                    "ACT - Soluções para Pessoas. Você pode usar formatação Markdown para adicionar informações neste espaço. Para mais informações acesse *https://www.markdownguide.org*",
-                    "©️")
-
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Cabeçalho")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
-
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-Divisor()
+def main():
+   Configurar_Pagina("WebApp01 - TESTE", "amplo", "auto", "https://docs.streamlit.io", "mailto:prof.massaki@gmail.com", "#### Aplicativo Teste", "©️")
+   Titulo("MEU PRIMEIRO APLICATIVO WEB")
+   st.header("Desenvolvido por Massaki de O. Igarashi")
+   
+   MKD(Versao(), 'left', 18, 'blue')
+      
+   MKD('⬜ Massaki01 - Biblioteca teste', 'left', 48, 'darkblue')
+      
+   Escrever('Hehehehe! Agora todos poderão fazer seu site e programar facilmente!', 'subcabecalho')
+   Escrever(' ')
+   Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
+   image = Image.open('desenvolvimento.jpg')
+   st.sidebar.image(image, width=300)
+	
+   CONTAINER = Container(True)
+   with CONTAINER: 
+      MKD('Biblioteca teste para facilitar o desenvolvimento de sites a qualquer pessoa leiga na Programação.', "justified", 20, "black")
     
-coluna1 = Colunas(3)
-with coluna1[0]:
-  Escrever("")
-with coluna1[1]:
-  Escrever("ACTlib Versão 0.1", "titulo")
-  nome = Ler(rotulo = "Nome:", nmax=30, tipo="padrao", info="Inserção de Nome", autocompletar=None, na_mudanca=None, args=None, kwargs=None, placeholder="Não esqueça de preencher seu nome", desabilitada="falso", visibilidade="visivel")
-  if nome:     
-    Escrever("Seja Bem vinda(o), " + nome + "!")
-with coluna1[2]:
-  Escrever("")
+   Barra_Lateral_Texto("Desenvolvido por Massaki Igarashi")
+
+   colunas = Colunas(4)       
+    
+   with colunas[0]:
+   	BTN1 = Botao_M("✔️ Botão 1")
+   with colunas[1]:
+        BTN2 = Botao_C("✔️ Botão 2", "gray")
+   with colunas[2]:
+        BTN3 = Botao_M("Botão 3")
+   with colunas[3]:   
+        BTN4 = Botao_C("✔️ Botão 4")
+       
+   if BTN1:
+        Escrever("Você clicou no Botão 1")
+   if BTN2:
+        Escrever("Você clicou no Botão 2")
+   if BTN3:
+        Escrever("Você clicou no Botão 3")       
+   if BTN4:
+        Escrever("Você clicou no Botão 4") 
+if __name__ == '__main__':
+	main()
