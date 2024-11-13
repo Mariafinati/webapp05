50d1060a72711ae9f9658917fe2ce5574a5dbd0d
Mariafinati <malvesfinati@gmail.com>
2024-11-13 20:50:37 -0300

    Update webapp05.py

--- webapp05.py
+++ webapp05.py
@@ -1,20 +1,8 @@
 import streamlit as st
-from PIL import Image
-from ACTlib01 import *
-
-def main():
-   Configurar_Pagina("WebApp01 - TESTE", "amplo", "auto", "https://docs.streamlit.io", "mailto:prof.massaki@gmail.com", "#### Aplicativo Teste", "©️")
-   Titulo("MEU PRIMEIRO APLICATIVO WEB")
-   st.header("Desenvolvido por Massaki de O. Igarashi") 
-   Escrever('Hehehehe! Agora todos poderão fazer seu site e programar facilmente!', 'subcabecalho')
-   Escrever(' ')
-   Escrever('Observação: Em desenvolvimento!', 'subcabecalho')
-   image = Image.open('desenvolvimento.jpg')
-   st.sidebar.image(image, width=300)
-   url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQtWlwpc4_SPunNRbY9MWEsLemJ4-kr7JK1OE8avUWMU7BULFMQNt6-bFkIsJ-_7nOvH3sFOSyOFkeb/pub?gid=1851101266&single=true&output=csv"
-   db = Ler_GooglePlanilha(url, coluna_indice = None)
-   db.columns = ['DataHora', 'Idade', 'Jundiaiense', 'Qualidade', 'Sonho']
-   Escrever(db)
-
-if __name__ == '__main__':
-	main()
+import requests
+from io import BytesIO
+import pandas as pd
+urlCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSE9-TpgudGYbsQtGsI6JNdusZYfCLZHNYvRbSd8NaT10h3-mosU-nhxrIi8gnnbU5v98v2my4hDQHd/pub?gid=883688627&single=true&output=csv"
+rD = requests,get(urlCSV)
+db = rD.content
+st.write(db)
